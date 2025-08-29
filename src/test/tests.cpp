9c6f1ff5179d5f20d1204095372157a2d60842ad
Latypov <ruslanlatypov14@gmail.com>
2025-08-29 05:21:01 +0500

    Creating tests (empty)

---
+++ src/test/tests.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //написать тесты
+    
+    cout << "[TEST]\tThe \"\" test was passed successfully\n";
+
+    cout << "[TESTS COMPLITED]" << std::endl;
+}
